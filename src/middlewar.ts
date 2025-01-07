db51d3f3185c0895d0dd875c7dbaf60c9b28b620
Elcio de Souza Monico <elcio.monico@gmail.com>
2025-01-07 16:59:44 -0300

    i

--- src/middlewar.ts
+++ src/middlewar.ts
@@ -18,7 +18,7 @@ export async function middleware(req: Request) {
     return NextResponse.next();
 }
 
-// Define as rotas que utilizarão este middleware
+//Define as rotas que utilizarão este middleware
 export const config = {
     matcher: '/api/cliente/:patch',
   }
