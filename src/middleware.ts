4331e9b356f942771487732dd19eacf012fdc84b
Elcio de Souza Monico <elcio.monico@gmail.com>
2025-02-03 14:08:06 -0300

    gerencie

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,7 +6,7 @@ const publicRoutes = [
     { path: "/dashboard/collaborator", whenAuthenticated: 'next' },
     { path: "/dashboard/collaborator/new", whenAuthenticated: 'next' },
     { path: "/dashboard/collaborator/editar/[id]", whenAuthenticated: 'next' },
-    { path: "/dashboard/edit", whenAuthenticated: 'next' },
+    { path: "/dashboard/edit/[id]", whenAuthenticated: 'next' },
     { path: "/dashboard/new", whenAuthenticated: 'next' },
     { path: "/api/auth/callback/credentials", whenAuthenticated: 'next' },
 ] as const;
@@ -16,26 +16,21 @@ export async function middleware(req: NextRequest) {
     const path = req.nextUrl.pathname;
     const publicCurrentRoute = publicRoutes.find((route) => route.path === path);
     const authToken = req.cookies.get("next-auth.session-token");
-    console.log("entrou aqui no middleware");
+
     if (!authToken && publicCurrentRoute) {
-        console.log("entrou aqui 1 if");
         return NextResponse.next();
     }
     if (!authToken?.value && !publicCurrentRoute) {
-        console.log("entrou aqui 2 if")
         const redirectUrl = req.nextUrl.clone();
-        console.log(redirectUrl);
         redirectUrl.pathname = REDIRECT_WHEN_NOT_AUTHENTICATED_ROUTE;
         return NextResponse.redirect(redirectUrl);
     }
     if (!authToken?.value && publicCurrentRoute && publicCurrentRoute.whenAuthenticated === 'redirect') {
-        console.log("entrou aqui 3 if")
         const redirectUrl = req.nextUrl.clone();
         redirectUrl.pathname = '/dashboard';
         return NextResponse.redirect(redirectUrl);
     }
     if (!authToken?.value && !publicCurrentRoute) {
-        console.log("entrou aqui 4 if")
        return NextResponse.next();
     }
    return NextResponse.next();
